2f7049cb84e1cc335a459f7221f40a7e932573e5
David Kanwisher <kanwisher@gmail.com>
2017-06-17 00:49:38 -0400

    the start of a beautiful wheel

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ function newGame(){
     console.log(currentPuzzle);
 
     document.getElementById('category').innerHTML = currentPuzzle.subject; //set puzzle category
-    if(currentPuzzle.rowA){
+    if(currentPuzzle.rowA){ //build out letters for each row by using function
         centerText("A", 12, 1);
     }
     if(currentPuzzle.rowB){
@@ -36,18 +36,23 @@ function newGame(){
 
     function centerText(rowNum, rowLength, letterStart){ //fill letters into the rows and attempt to center them
         let wordSplit = currentPuzzle["row" + rowNum].split('');
-        let firstLetter = Math.floor((rowLength - wordSplit.length) / 2) + letterStart|| letterStart; //yay, I made up some math stuff
+        let firstLetterIdx = Math.floor((rowLength - wordSplit.length) / 2) + letterStart|| letterStart; //yay, I made up some math stuff to center them
             
-        wordSplit.forEach(function(elem){ //loops array
+        wordSplit.forEach(function(elem){ //loops array for each letter
             let element = document.createElement("p");
             element.innerHTML = elem;
-            element.className += " blank"
-            document.getElementById(`item${firstLetter}`).appendChild(element);
-            if(elem !== " "){
-                element.parentElement.className += " unsolved"
+            
+            document.getElementById(`item${firstLetterIdx}`).appendChild(element); //give it an id of what square it is in
+            if (/[A-Z]/.test(elem)){
+                element.style.opacity = 0;
+                element.parentElement.className += " puzzlePiece"
+                element.className += " blank"
             }
-            element.style.opacity = 0;
-            firstLetter++
+            else if(elem !== " " ){
+                element.parentElement.className += " puzzlePiece" //if it's a symbol SHOW
+            }
+            
+            firstLetterIdx++
         })
     }
 
@@ -57,7 +62,7 @@ function newGame(){
 
         while(!test){ //stop loop when boolean flag is changed
             currentPuzzle = puzzleBank[Math.floor(Math.random() * puzzleBank.length - 1)] //generate random puzzle
-            if (currentPuzzle.rowA.length > 12 || //throw out puzzles that may not fit the board
+            if (currentPuzzle.rowA.length > 12 || //throw out puzzles that may not fit the board, being lazy here
                 currentPuzzle.rowB.length > 12 ||
                 currentPuzzle.rowC.length > 12 ||
                 currentPuzzle.rowD.length > 12) {
@@ -78,18 +83,25 @@ function newGame(){
     document.addEventListener("keyup", function(e){
         let userGuess = e.key;
         let nodeList = document.querySelectorAll(".blank");
-        console.log(nodeList);
+        let delayMulti = 0;
 
-        // for (let i = 0, len = nodeList.length; i < len; i++) {
-        //     
-        //         nodeList[i].className = "revealed";
-        //     }
-        // }
-        Array.prototype.forEach.call (nodeList, function (node) { //stealing array method for nodeList
+
+        Array.prototype.forEach.call (nodeList, function (node, idx, array) { //stealing array method for nodeList
             if(e.key.toLowerCase() === node.innerHTML.toLowerCase()){
-                node.className = "revealed";
-            }
+                setTimeout(function(){
+                    node.className = "revealed"
+                    winCheck(array);
+                },1000 * delayMulti) //throw an incrementing delay as it iterates, so multiple letters are not revealed at the same time            
+            delayMulti++
+            }        
         })
+
+        function winCheck(array){ //this works
+            if(array.length === 1){ //if last item was just changed to revealed 
+                console.log(array);
+                console.log("You win!!!");
+            }
+        }
     })
 }
 newGame();
